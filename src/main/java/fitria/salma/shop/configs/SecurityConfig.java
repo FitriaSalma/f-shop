97c6335f239a736147d42c043fc7db382ad458ad
Fitria Salma <47936349+FitriaSalma@users.noreply.github.com>
2024-03-28 13:00:10 +0700

    security config

---
+++ src/main/java/fitria/salma/shop/configs/SecurityConfig.java
@@ -0,0 +1,8 @@
+package fitria.salma.shop.configs;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+}
