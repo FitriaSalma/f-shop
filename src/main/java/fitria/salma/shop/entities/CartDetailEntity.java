c63a074770ce96553053782c47aea746f113d00b
Fitria Salma <47936349+FitriaSalma@users.noreply.github.com>
2024-04-02 14:21:58 +0700

    update cart detail entity

--- src/main/java/fitria/salma/shop/entities/CartDetailEntity.java
+++ src/main/java/fitria/salma/shop/entities/CartDetailEntity.java
@@ -1,5 +1,72 @@
 package fitria.salma.shop.entities;
 
-public class CartDetailEntity {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "cart_detail")
+public class CartDetailEntity extends BaseProperties {
+
+	@Id
+	@Column(nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_cart_detail;
+	
+	@Column
+	private Long id_cart;
+	
+	@Column
+	private String item_code;
+	
+	@Column
+	private Integer quantity;
+	
+	@Column
+	private Integer subtotal_price;
+
+	public Long getId_cart_detail() {
+		return id_cart_detail;
+	}
+
+	public void setId_cart_detail(Long id_cart_detail) {
+		this.id_cart_detail = id_cart_detail;
+	}
+
+	public Long getId_cart() {
+		return id_cart;
+	}
+
+	public void setId_cart(Long id_cart) {
+		this.id_cart = id_cart;
+	}
+
+	public String getItem_code() {
+		return item_code;
+	}
+
+	public void setItem_code(String item_code) {
+		this.item_code = item_code;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public Integer getSubtotal_price() {
+		return subtotal_price;
+	}
+
+	public void setSubtotal_price(Integer subtotal_price) {
+		this.subtotal_price = subtotal_price;
+	}
+	
+	
 }
